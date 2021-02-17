92783753e8ae1dfa843b7e14adab58b0bacaf5bf
vincenteof <vincenteof@qq.com>
2021-02-17 11:15:57 +0800

    feat: update

--- src/components/clock/index.tsx
+++ src/components/clock/index.tsx
@@ -19,12 +19,20 @@ const Clock: FC<ClockProps> = () => {
   const now = dayjs()
   const hour = now.hour()
   const minute = now.minute()
+  const dateStr = now.format('MM月DD日')
+  const weekStr = weekdayCh(now.day() as any)
 
   return (
     <div className={cls('clock', mode === 'dark' && 'dark')}>
-      <span>{formatNumber(hour)}</span>
-      <span>:</span>
-      <span>{formatNumber(minute)}</span>
+      <div className="hour-minute">
+        <span>{formatNumber(hour)}</span>
+        <span>:</span>
+        <span>{formatNumber(minute)}</span>
+      </div>
+      <div className="week-date">
+        <div>{dateStr}</div>
+        <div>{weekStr}</div>
+      </div>
     </div>
   )
 }
@@ -34,6 +42,19 @@ const formatNumber = (num: number) => {
     return `0${num}`
   }
   return `${num}`
+}
+
+const weekdayCh = (num: 0 | 1 | 2 | 3 | 4 | 5 | 6) => {
+  const mapping = {
+    0: '星期日',
+    1: '星期一',
+    2: '星期二',
+    3: '星期三',
+    4: '星期四',
+    5: '星期五',
+    6: '星期六',
+  }
+  return mapping[num]
 }
 
 export default Clock
